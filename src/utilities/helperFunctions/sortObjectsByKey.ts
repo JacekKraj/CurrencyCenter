c61da251718a1f34994e9579aaae376b8c327752
Jacek Krajewski <jacekkrajewski12@wp.pl>
2022-05-31 15:07:14 +0200

    change sortObjectsByKey helperFunctions
    make object values lowercase if they are strings

--- src/utilities/helperFunctions/sortObjectsByKey.ts
+++ src/utilities/helperFunctions/sortObjectsByKey.ts
@@ -1,8 +1,20 @@
-export const sortObjectsByKey = <T extends object, K extends keyof T>(elements: T[], key: K): T[] => {
-  const compare = (a: T, b: T) => {
-    if (a[key] > b[key]) return 1;
+const getFormatedValues = <T>(value1: T, value2: T) => {
+  if (typeof value1 === 'string' && typeof value2 === 'string')
+    return {
+      value1: value1.toLowerCase(),
+      value2: value2.toLowerCase(),
+    };
 
-    if (a[key] < b[key]) return -1;
+  return { value1, value2 };
+};
+
+export const sortObjectsByKey = <T extends object, K extends keyof T>(elements: T[], key: K): T[] => {
+  const compare = (object1: T, object2: T) => {
+    const { value1, value2 } = getFormatedValues(object1[key], object2[key]);
+
+    if (value1 > value2) return 1;
+
+    if (value1 < value2) return -1;
 
     return 0;
   };
