4cf1c2a792301a54852ec9c52da4d0ab6e75d6b1
Jacek Krajewski <jacekkrajewski12@wp.pl>
2022-06-27 13:23:36 +0200

    fix banks comparison bug
    fix bug that if banks are sorted by any condition and then currency
    changes, this condition keeps being true in state, although it
    shouldnt anymore, add style changes

--- src/components/banksComparison/comparisonTable/tableHeader/TableHeader.tsx
+++ src/components/banksComparison/comparisonTable/tableHeader/TableHeader.tsx
@@ -1,28 +1,38 @@
 import React, { SetStateAction, Dispatch } from 'react';
+import classnames from 'classnames';
 
 import { SortingConditions } from '../ComparisonTable';
 import classes from './tableHeader.module.scss';
 
 interface Props {
-  setSortingCondition: Dispatch<SetStateAction<SortingConditions>>;
+  sortingCondition: {
+    set: Dispatch<SetStateAction<SortingConditions>>;
+    current: SortingConditions;
+  };
 }
 
-const TableHeader: React.FC<Props> = ({ setSortingCondition }) => {
+const TableHeader: React.FC<Props> = ({ sortingCondition }) => {
+  const { set, current } = sortingCondition;
   const handleClick = (condition: SortingConditions) => {
-    setSortingCondition(condition);
+    set(condition);
   };
 
-  const renderButtons = () => {
+  const renderButtons = React.useMemo(() => {
     return Object.values(SortingConditions).map((condition) => {
       return (
-        <button onClick={() => handleClick(condition)} key={condition} data-test={`sort-button-${condition}`}>
+        <button
+          className={classnames(condition === current && classes.active)}
+          onClick={() => handleClick(condition)}
+          key={condition}
+          data-test={`sort-button-${condition}`}
+        >
           {condition}
         </button>
       );
     });
-  };
+  }, [current]);
 
-  return <div className={classes.tableHeader}>{renderButtons()}</div>;
+  return <div className={classes.tableHeader}>{renderButtons}</div>;
 };
 
 export default TableHeader;
